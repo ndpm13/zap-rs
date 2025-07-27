85fb9fcc50d16f8b8147c117521b836ab750cd98
Naz <ndpm13@ch-naseem.com>
2025-07-27 14:37:16 +0100

    ✨feat: add AppImage index persistence

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,8 @@
 use serde::Serialize;
 use std::path::PathBuf;
+use tokio::fs;
+
+use crate::{index_dir};
 
 #[derive(Debug, Serialize)]
 pub struct AppImage {
@@ -17,4 +20,15 @@ pub struct Source {
 #[derive(Debug, Serialize)]
 pub struct SourceMetadata {
     pub url: String,
+}
+
+impl AppImage {
+    pub async fn save_to_index(&self, appname: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let index_file = &index_dir().join(format!("{appname}.json"));
+
+        let json = serde_json::to_string_pretty(self)?;
+        fs::write(index_file, json).await?;
+
+        Ok(())
+    }
 }
